f3c070f6c3df2b09e1281a28031b1cb3e2f51f1b
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2020-06-30 16:22:56 +0100

    add basic math operator functions

---
+++ app.js
@@ -0,0 +1,30 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    return number1 / number2;
+}
+
+function operate(operator, number1, number2) {
+    switch(operator) {
+        case '+':
+            return add(number1, number2);
+        case '-':
+            return subtract(number1, number2);
+        case '*':
+            return multiply(number1, number2);
+        case '/':
+            return divide(number1, number2);
+        default:
+            return 'Invalid operator';
+    }
+}
