a536d5c7232b1854b7d5dc9523dce35beea92cdd
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2020-07-11 14:44:45 +0100

    refactor operate function

--- app.js
+++ app.js
@@ -91,32 +91,18 @@ function updateDisplay(value) {
     displayText.textContent = value;
 }
 
-function add(number1, number2) {
-    return number1 + number2;
-}
-
-function subtract(number1, number2) {
-    return number1 - number2;
-}
-
-function multiply(number1, number2) {
-    return number1 * number2;
-}
-
-function divide(number1, number2) {
-    return number1 / number2;
-}
-
 function operate(operator, number1, number2) {
+    number1 = parseFloat(number1);
+    number2 = parseFloat(number2);
     switch(operator) {
         case '+':
-            return add(number1, number2);
+            return number1 + number2;
         case '−':
-            return subtract(number1, number2);
+            return number1 - number2;
         case '×':
-            return multiply(number1, number2);
+            return number1 * number2;
         case '÷':
-            return divide(number1, number2);
+            return number1 / number2;
         default:
             return 'Invalid operator';
     }
